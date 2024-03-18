91113ab952d5104f6823454dff5989aa5492ba37
benlee1284 <ben.lee@kayenta.io>
2024-03-18 14:17:28 +0000

    Add test for calculate shipping in the UK

--- test/test_calculate_shipping.py
+++ test/test_calculate_shipping.py
@@ -1,5 +1,8 @@
+import pytest
+
 from src.countries import Country
 from src.regions.constants import Regions
+from src.shipping import calculate_shipping
 
 
 def region_fetcher_test_double(country: str) -> str:
@@ -12,3 +15,19 @@ def region_fetcher_test_double(country: str) -> str:
     else:
         raise NotImplementedError(f"Country {country} not implemented")
 
+
+@pytest.mark.parametrize(
+    "order_total, expected_shipping",
+    [
+        (1.0, 4.99),
+        (99.99, 4.99),
+        (100.00, 0.0),
+    ],
+)
+def test_calculate_shipping_uk(order_total: float, expected_shipping: float):
+    shipping = calculate_shipping(
+        region_fetcher=region_fetcher_test_double,
+        country=Country.UNITED_KINGDOM.value,
+        order_total=order_total,
+    )
+    assert shipping == expected_shipping
