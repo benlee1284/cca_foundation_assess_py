04ae633096c6fb3d0b64e63718a67ba954cdd238
benlee1284 <ben.lee@kayenta.io>
2024-03-18 16:04:30 +0000

    Implement list orders for product function

--- src/history.py
+++ src/history.py
@@ -1,7 +1,28 @@
 from dataclasses import dataclass
-from order import Order
+from src.order import Order
+from src.product import Product
 
 
 @dataclass
 class SalesHistory:
     orders: list[Order]
+
+    def list_orders_for_product(self, product: Product) -> list[Order]:
+        orders_containing_product = [
+            order
+            for order in self.orders
+            if any(item.product == product for item in order.items)
+        ]
+
+        filtered_orders = []
+
+        for order in orders_containing_product:
+            relevant_items = [item for item in order.items if item.product == product]
+            order_with_only_relevant_items = Order(
+                shipping_address=order.shipping_address,
+                items=relevant_items,
+            )
+
+            filtered_orders.append(order_with_only_relevant_items)
+
+        return filtered_orders
