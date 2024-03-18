9cc5acbabe96d5ffc50c46a5e211ab44dbb7659f
benlee1284 <ben.lee@kayenta.io>
2024-03-18 15:03:35 +0000

    Implement check stock function

--- src/warehouse.py
+++ src/warehouse.py
@@ -12,3 +12,12 @@ class Entry:
 @dataclass
 class Warehouse:
     catalogue: list[Entry]
+
+    def check_stock(self, product: Product) -> int:
+        product_entry = next(
+            filter(lambda entry: entry.product == product, self.catalogue), None
+        )
+        if product_entry is None:
+            return 0
+
+        return product_entry.stock
